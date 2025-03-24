fd3a87bd55c6645cdbc1d6468ad077d08749f95b
Gabriel <gabrielnunesdacostaanselmo@gmail.com>
2025-03-24 08:04:51 -0300

    Programa Finalizado

--- Program.cs
+++ Program.cs
@@ -6,19 +6,19 @@ Console.WriteLine("Preecha a ficha com informações do seu pet\n");
 
 Console.WriteLine("FICHA: ");
 
-Console.WriteLine("Digite a espécie: ");
+Console.Write("Digite a espécie: ");
 especie = Console.ReadLine()!.ToUpper();
 
-Console.WriteLine("Digite a raça: ");
+Console.Write("Digite a raça: ");
 raca = Console.ReadLine()!.ToUpper();
 
-Console.WriteLine("Digite a alcunha: ");
+Console.Write("Digite a alcunha: ");
 alcunha = Console.ReadLine()!.ToUpper();
 
-Console.WriteLine("Digite a idade: ");
-idade = int.Parse(Console.ReadLine() ??"");
+Console.Write("Digite a idade: ");
+idade = int.Parse(Convert.ToString(Console.ReadLine()!));
 
-Console.WriteLine("Digite a pelagem: ");
+Console.Write("Digite a pelagem: ");
 pelagem = Console.ReadLine()!.ToUpper();
 
 Console.Clear();
@@ -33,15 +33,15 @@ Console.WriteLine("|               Pet Hotel \"Nem um pio\"                    |
 Console.WriteLine("+=========================================================+");
 
 Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine($"| Espécie: {especie,-18} | Raça: {raca,-19} |");
+Console.WriteLine($"| Espécie: {especie.PadLeft(18,'.')} | Raça: {raca.PadLeft(19,'.')} |");
 
 
      Console.ForegroundColor = ConsoleColor.Gray;
 Console.WriteLine("+=========================================================+");
 
     Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine($"| Atende pela alcunha de: {alcunha,-31} |");
-Console.WriteLine($"| Idade: {idade,-2}ano(s) | Pelagem/cor: {pelagem,-24} |");
+Console.WriteLine($"| Atende pela alcunha de: {alcunha.PadLeft(31,'.')} |");
+Console.WriteLine($"| Idade: {idade}ano(s) | Pelagem/cor: {pelagem.PadLeft(24,'.')} |");
 
     Console.ForegroundColor = ConsoleColor.Gray;
 Console.WriteLine("+=========================================================+");
